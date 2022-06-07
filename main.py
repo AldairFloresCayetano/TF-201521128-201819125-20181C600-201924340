bd95f094f3be66bcc3a3f668de79e31ab9e5a15c
AldairFloresCayetano <u201924340@upc.edu.pe>
2022-06-07 05:44:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
   Origen -> -12.0459308;-77.0427831
   Destino -> -12.0460958;-77.0430896
 """
-import math
+
 
 class Nodo:
     def __init__(self, id, nombre, intersecciones) -> None:
@@ -67,51 +67,27 @@ def obtener_formato_calles(lista, data):
 
 def obtener_formato_intersecciones(lista, data):
     lista.append(
-        (   #item
+        (
             int(data[0]),
-            #Id de la calle
             int(data[1]),
-            #Nombre de la calle
             data[2],
-            #Id de la calle Origen
             int(data[3]),
-             #Id de la calle final 
             int(data[4]),
-               #Id origen Intersección
             int(data[5]),
-            #Id destino Intersección
             int(data[6]),
-            #Distancia en Km/h entre Id origen de la intersección/ Id destino de la intersección
             float(data[7]),
-            #Velocidad km/h permitida
             int(data[8]),
-            #Costo 1
             float(data[9]),
-            #Costo 2
             float(data[10]),
-            #Latitud de 6
             float(data[11]),
-            #Longitud de 6
             float(data[12]),
-            #Latitud de 7
             float(data[13]),
-            #Longitud de 7
-            float(data[14]),
-             
-            #Costo 1 modificado: Distancia(km)/velocidad(km/h) 
-            float(data[6])/ float(data[7]),
-            #Costo 2 modificado: (Distancia entre 2 puntos x1000000)= raiz[ (latitud1-latitud2)^2+(longitud1-longitud2)^2]X1000000
-           (( math.sqrt ((float(data[11])-float(data[13]))**2+  (float(data[12])- float(data[14]))**2))*1000000),
-
-            #Costo Factor trafico:Distancia*1000(km)/velocidad((100-h)/100) 
-             float(data[7])/ (int(data[8])*((100-hora)/100))
         )
     )
-    
 
 
 calles = leer_archivo_formato("assets/Lima-calles.csv", obtener_formato_calles)
-hora =  int(input("Digite la hora: "))
+
 intersecciones = leer_archivo_formato("assets/Lima-intersecciones.csv", obtener_formato_intersecciones)
 
 Graph = Grafo(calles, intersecciones)
